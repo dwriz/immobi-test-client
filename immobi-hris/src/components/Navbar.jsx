6cc909b8b8621f24663ca7964359953fea50dd9e
dwriz <dwikyrizkya27@gmail.com>
2024-07-18 08:52:34 +0700

    add tailwind, refactor navbar

--- immobi-hris/src/components/Navbar.jsx
+++ immobi-hris/src/components/Navbar.jsx
@@ -1,8 +1,32 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <>
-    <h1>This is Navbar</h1>
-    </>
+    <nav className="bg-gray-800 p-4 flex justify-between items-center">
+      <div className="text-white text-lg">
+        <Link to="/">Immobi HRIS</Link>
+      </div>
+      <div className="space-x-4">
+        <Link
+          to="/add-department"
+          className="text-white hover:bg-gray-700 px-3 py-2 rounded"
+        >
+          Add Department
+        </Link>
+        <Link
+          to="/add-position"
+          className="text-white hover:bg-gray-700 px-3 py-2 rounded"
+        >
+          Add Position
+        </Link>
+        <Link
+          to="/add-employee"
+          className="text-white hover:bg-gray-700 px-3 py-2 rounded"
+        >
+          Add Employee
+        </Link>
+      </div>
+    </nav>
   );
 }
 
